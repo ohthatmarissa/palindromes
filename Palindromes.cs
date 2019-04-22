59e89799e9f28c9cef163b664a0150df674daea6
Marissa Perry <ohthatmarissa@gmail.com>
2019-04-22 16:07:01 -0700

    initial commit

---
+++ Palindromes.cs
@@ -0,0 +1,23 @@
+using System;
+namespace palindromecheck {
+   class Program {
+      static void Main(string[] args) {
+
+         string string1, rev;
+         Console.WriteLine("Type a word to see if it is a Palindrome");
+         string1 = Console.ReadLine();
+         char[] ch = string1.ToCharArray();
+
+         Array.Reverse(ch);
+         rev = new string(ch);
+
+         bool b = string1.Equals(rev, StringComparison.OrdinalIgnoreCase);
+         if (b == true) {
+            Console.WriteLine("" + string1 + " is a Palindrome!");
+         } else {
+            Console.WriteLine(" " + string1 + " is not a Palindrome!");
+         }
+         Console.Read();
+      }
+   }
+}
